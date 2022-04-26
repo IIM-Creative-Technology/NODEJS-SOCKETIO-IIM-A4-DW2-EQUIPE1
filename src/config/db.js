c1e61cdd33d21431ede7f4447420eea19bda716e
MathiasGilles <fb132fce5>
2022-04-26 11:53:52 +0200

    add configuration for sequelize in db.js

---
+++ src/config/db.js
@@ -0,0 +1,6 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    dialect: 'postgres'
+});
