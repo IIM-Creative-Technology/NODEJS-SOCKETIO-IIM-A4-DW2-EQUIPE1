9c9ba4a625213a21432d8d2c373adc71969dc966
Benjamin Pichon <benpichon@hotmail.fr>
2022-04-26 16:05:57 +0200

    FIX try catch connect to db (kill node server if error)

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const db = require('./src/config/db')
 const app = express();
 const port = 3000;
 
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
   try {
     await db.sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    console.log(`Example app listening on port ${port}`);
   } catch (error) {
     console.error('Unable to connect to the database:',error);
+    server.close()
   }
-  console.log(`Example app listening on port ${port}`);
 });
