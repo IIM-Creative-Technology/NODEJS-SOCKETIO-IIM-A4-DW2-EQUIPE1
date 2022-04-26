0db5e52693ef282db26f131ff4900ee059a1cf31
MathiasGilles <fb132fce5>
2022-04-26 15:24:29 +0200

    add test authenticate to the database

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const db = require('./src/config/db')
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(db.sequelize)
+app.listen(port, async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:',error);
+  }
   console.log(`Example app listening on port ${port}`);
 });
