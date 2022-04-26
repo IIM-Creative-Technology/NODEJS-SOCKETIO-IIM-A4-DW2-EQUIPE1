a0f8e04c912e53951957e4ae6b454bedf295217d
Benjamin Pichon <benpichon@hotmail.fr>
2022-04-26 12:13:46 +0200

    add pg to the package.json

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const db = require('./src/config/db')
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
+  console.log(db.sequelize)
   console.log(`Example app listening on port ${port}`);
 });
