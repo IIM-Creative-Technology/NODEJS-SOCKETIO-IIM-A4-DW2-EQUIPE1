fe6be746f9f069e62173449c2f01aab9c4c9c799
victorgbn <victor.gaubin@gmail.com>
2022-04-27 17:01:08 +0200

    [IMPORT FILES] added import files function

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const swaggerDocument = require("./docs/swagger.json");
 const userRouter = require("./src/routes/users");
 const docRouter = express.Router();
 const cookieParser = require("cookie-parser");
+const multer = require('multer');
+const path = require('path');
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +24,39 @@ docRouter.get("/swagger", swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", userRouter);
 app.use("/docs", docRouter);
+
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myFile');
+
+function checkFileType(file, cb){
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+app.use(express.static('./public'));
+
+app.post('/upload', (req, res) => {
+  upload(req, res)
+});
 
 const server = app.listen(port, async () => {
   try {
