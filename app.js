e03163d0fb4a16f9d01a3503c09d196aa09fc003
EidanU <eidanughetti.contact@gmail.com>
2022-04-27 16:59:09 +0200

    [feature] install chat with socket.io

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const swaggerDocument = require("./docs/swagger.json");
 const userRouter = require("./src/routes/users");
 const docRouter = express.Router();
 const cookieParser = require("cookie-parser");
-const socket = require('socket.io');
+
+const http = require('http').Server(app);
+const io = require("socket.io")(http);
+let userList = [];
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,16 +18,34 @@ app.use(cookieParser());
 app.get("/api/ping", (req, res) => {
   res.send("API is up and running!");
 });
+
 app.get("/", (req, res)=>{
-res.sendFile(__dirname + '/template/index.html')
+  res.sendFile(__dirname + '/template/index.html')
 })
+
+app.get("/chat", (req, res)=>{
+  res.sendFile(__dirname + '/template/chat/chat.html')
+})
+
 docRouter.use("/swagger", swaggerUi.serve);
 docRouter.get("/swagger", swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", userRouter);
 app.use("/docs", docRouter);
 
-const server = app.listen(port, async () => {
+io.on('connection', (socket) => {
+  socket.on('login', (userInfo) => {
+    userList.push(userInfo);
+    io.emit('userList', userList);
+  })
+  // Exit (built-in event)
+  socket.on('disconnect', () => {
+    userList = userList.filter(item => item.id != socket.id)
+    io.emit('quit', socket.id)
+  })
+})
+
+http.listen(port, async () => {
   try {
     await db.sequelize.authenticate();
     console.log("Connection has been established successfully.");
@@ -35,9 +56,5 @@ const server = app.listen(port, async () => {
   }
 });
 
-const io = socket(server);
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
-});
 
